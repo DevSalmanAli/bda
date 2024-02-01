4b6cfc562ccb4925cc06571a32409aa03322165b
DevSalmanAli <158503239+DevSalmanAli@users.noreply.github.com>
2024-02-02 00:23:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    var message = "You make every day brighter. Happy Birthday, my love My Jaan My Shaista Ali!";
+    document.getElementById("specialMessage").innerHTML = message;
+  }
+  
