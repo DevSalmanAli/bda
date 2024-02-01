4a7f20bb20da78c3cb92e39bd491d9903d254fdf
DevSalmanAli <158503239+DevSalmanAli@users.noreply.github.com>
2024-02-02 00:36:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showMessage() {
-    var message = "I was boken and you taught me how to love again with broken heart I was totally lost and found me, I am glad to have you back in my life. You make every day brighter. Happy Birthday, My love My Shaista Ali! Bas hamesha khush raho hanste raho. Again, I love you my Birth Day Girl My Shaista!";
-    document.getElementById("specialMessage").innerHTML = message;
-  }
-  
